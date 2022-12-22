ff39de08fbc567c88adce933c3a182565bd45ca9
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-12-22 17:33:11 +0000

    First commit

---
+++ settings.py
@@ -0,0 +1,19 @@
+# Settings class.
+# Instead of adding settings throughout the code, let's write a module called
+# settings that contains a class called Settings to store all these values in
+# one place.
+
+# This approach allows us work with just one settings object any time we need
+# to access an individual setting. This also makes it easier to modify the
+# game's appeareance and behaviour as our project grows.
+
+# To modify the game, we'll simply change some values in settings.py.
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
