442ae09c9b2fa22313530a813d7685eb70b881f9
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-12-23 15:57:05 +0000

    Add ship movement
    Ship can now move left and right according to keypresses.

--- ship.py
+++ ship.py
@@ -12,6 +12,8 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -20,7 +22,26 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
-    
+
+        # Store a decimal value of the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's x value, not the rect
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        # Update rect ob ject from self.x
+        self.rect.x = self.x
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
